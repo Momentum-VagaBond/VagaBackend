f815c73ac285476295e6b566f55172978c3d6583
Emily Starr <emilyflo.starr@gmail.com>
2022-04-23 12:57:34 -0400

    basic model stuff and pushing to be able to register and login users

--- VagaBond/settings.py
+++ VagaBond/settings.py
@@ -142,12 +142,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly', 
-        'rest_framework.authentication.TokenAuthentication'
-    ]
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+    ],
+        'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        )
 }
 
 
--- core/models.py
+++ core/models.py
@@ -1,7 +1,50 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 class User(AbstractUser):
 
+    def __repr__(self):
+        return f"<User username={self.username} pk={self.pk}>"
+
     def __str__(self):
         return self.username
+
+class Contacts(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='contacts', default=True)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=75)
+
+    def __str__(self):
+        return self.user
+
+class Trip(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='trips')
+    location = models.CharField(max_length=150, blank=True)
+    date_added = models.DateTimeField
+
+    def __str__(self):
+        return self.location
+
+
+class Log(models.Model):
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name='trip_log')
+    location = models.CharField(max_length=75)
+    latitude = models.FloatField('latitude')
+    longitude = models.FloatField('longitude')
+    details = models.TextField(max_length=250)
+
+
+    def __str__(self):
+        return self.log_number
+
+
+
+class Comment(models.Model):
+    log = models.ForeignKey(Log, on_delete=models.CASCADE, related_name='log_comments')
+    comments = models.TextField(max_length=250)
+    date_commented = models.DateTimeField
+
+    def __str__(self):
+        return self.comments
