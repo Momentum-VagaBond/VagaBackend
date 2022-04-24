0b5726a8223168938ab9c30bc564b6322ab2acf4
Alex Jordan <AlJord91@gmail.com>
2022-04-23 23:00:41 -0400

    added start date, user serializer

--- api/serializers.py
+++ api/serializers.py
@@ -3,17 +3,18 @@ from rest_framework import serializers
 
 
 
-class UserSerializer(serializers.ModelSerializer):
-    def get_user(self, obj):
-        return obj.user.username
-
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    trips = serializers.HyperlinkedIdentityField(view_name='my-trips', format='html')
+    username = serializers.SlugRelatedField(slug_field='username', read_only='True', source='user')
+    user_first_name = serializers.SlugRelatedField(slug_field='first_name', read_only='True', source='user')
+    user_last_name = serializers.SlugRelatedField(slug_field='last_name', read_only='True', source='user')
     class Meta:
         model=User
         fields=(
-            ''
-
-
-
-
-
+            'id',
+            'username',
+            'user_first_name',
+            'user_last_name',
+            'bio',
+            'trips',
         )
