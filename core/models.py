56c46b793779eb64450f9ba1ec14f693d19e14f7
Alex Jordan <AlJord91@gmail.com>
2022-04-28 20:23:44 -0400

    can convert address to lat/long

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 #changes
+import geocoder
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
@@ -62,22 +63,27 @@ class Trip(models.Model):
             recipient_list=email_list
         )
 
-
+mapbox_token = 'pk.eyJ1IjoiZW1pbHlmbG8iLCJhIjoiY2wyZGRsNG9hMHk0aDNicGR1bjhxZGZmdyJ9.OwfzAfjxswxUss6pTmNVUQ'
 
 class Log(models.Model):
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name='trip_logs')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='logs', default=True)
     location = models.CharField(max_length=75)
-    latitude = models.FloatField('latitude')
-    longitude = models.FloatField('longitude')
+    latitude = models.FloatField('latitude', blank=True, null=True)
+    longitude = models.FloatField('longitude', blank=True, null=True)
     details = models.TextField(max_length=250)
     start = models.BooleanField(default=False)
     date_logged = models.DateTimeField(auto_now_add=True)
 
-
-
     def __str__(self):
         return self.location
+    
+    def save(self, *args, **kwargs):
+        g = geocoder.mapbox(self.location, key=mapbox_token)
+        g = g.latlng
+        self.latitude = g[0]
+        self.longitude = g[1]
+        return super(Log, self).save(*args, **kwargs)
 
 
 class Comment(models.Model):
